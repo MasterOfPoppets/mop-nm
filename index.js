a10f93ab239a5210d8b30b7e49d09ced160263a7
Gareth Hughes <gurrkin@googlemail.com>
2015-08-30 01:08:06 +0100

    [Update] Init

---
+++ index.js
@@ -0,0 +1,10 @@
+var metalsmith = require('metalsmith');
+var path = require('path');
+
+metalsmith(__dirname)
+	.destination(path.join(process.cwd(), process.argv[2]))
+	.build(function onBuild(err) {
+		if (err) {
+			throw err;
+		}
+	});
